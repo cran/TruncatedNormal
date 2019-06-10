5e3de680feb502cefe1656dd5ba1b70faf5ec1e7
Leo Belzile <belzilel@gmail.com>
2019-06-10 14:10:15 +0000

    version 2.0

--- R/cholperm.R
+++ R/cholperm.R
@@ -1,65 +1,33 @@
-cholperm <-
-  function( Sig, l, u ){
-    #  Computes permuted lower Cholesky factor L for Sig
-    #  by permuting integration limit vectors l and u.
-    #  Outputs perm, such that Sig(perm,perm)=L%*%t(L).
-    #
-    # Reference: 
-    #  Gibson G. J., Glasbey C. A., Elston D. A. (1994),
-    #  "Monte Carlo evaluation of multivariate normal integrals and
-    #  sensitivity to variate ordering", 
-    #  In: Advances in Numerical Methods and Applications, pages 120--126
-    eps=10^-10; # round-off error tolerance
-    d=length(l);perm=1:d; # keep track of permutation
-    L=matrix(0,d,d);z=rep(0,d);
-    for (j in 1:d){
-      pr=rep(Inf,d); # compute marginal prob.
-      I=j:d; # search remaining dimensions
-      D=diag(Sig);
-      if (j >2){
-        s=D[I]-L[I,1:(j-1)]^2%*%rep(1,j-1)
-      } else if (j==2){
-        s=D[I]-L[I,1]^2
-      } else {
-        s=D[I]
-      }
-      s[s<0]=eps;s=sqrt(s);
-      if (j >2){
-        cols=L[I,1:(j-1)]%*%z[1:(j-1)]
-      } else if (j==2){
-        cols=L[I,1]*z[1]
-      } else {
-        cols=0
-      }
-      tl=(l[I]-cols)/s;tu=(u[I]-cols)/s;pr[I]=lnNpr(tl,tu);
-      # find smallest marginal dimension
-      k=which.min(pr);
-      # flip dimensions k-->j
-      jk=c(j,k);kj=c(k,j);
-      Sig[jk,]=Sig[kj,];Sig[,jk]=Sig[,kj]; # update rows and cols of Sig
-      L[jk,]=L[kj,]; # update only rows of L
-      l[jk]=l[kj];u[jk]=u[kj]; # update integration limits
-      perm[jk]=perm[kj]; # keep track of permutation
-      # construct L sequentially via Cholesky computation
-      s=Sig[j,j]-sum(L[j,1:(j-1)]^2);
-      if (s<(-0.001)){
-        stop('Sigma is not positive semi-definite')
-      }
-      s[s<0]=eps;L[j,j]=sqrt(s);
-      if (j<d){
-        if (j >2){
-          L[(j+1):d,j]=(Sig[(j+1):d,j]-L[(j+1):d,1:(j-1)]%*%L[j,1:(j-1)])/L[j,j];
-        } else if (j==2){
-          L[(j+1):d,j]=(Sig[(j+1):d,j]-L[(j+1):d,1]*L[j,1])/L[j,j];
-        } else if (j==1){
-          L[(j+1):d,j]=Sig[(j+1):d,j]/L[j,j];
-        }
-      }
-      # find mean value, z(j), of truncated normal:
-      tl=(l[j]-L[j,1:j]%*%z[1:j])/L[j,j];
-      tu=(u[j]-L[j,1:j]%*%z[1:j])/L[j,j];
-      w=lnNpr(tl,tu); # aids in computing expected value of trunc. normal
-      z[j]=(exp(-.5*tl^2-w)-exp(-.5*tu^2-w))/sqrt(2*pi);
-    }
-    return(list(L=L,l=l,u=u,perm=perm))
+#' Cholesky decomposition for Gaussian distribution function with permutation
+#' 
+#' This function computes the Cholesky decomposition of a covariance matrix
+#' \code{Sigma} and returns a list containing the permuted bounds for integration. 
+#' The prioritization of the variables follows either the rule proposed in Gibson, Glasbey and Elston (1994),
+#' reorder variables to have outermost variables with smallest expected values. The alternative is the scheme proposed
+#' in Genz and Bretz (2009) that minimizes the variance of the truncated Normal variates.
+#' 
+#' The list contains an integer vector \code{perm} with the indices of the permutation, which is such that
+#' \code{Sigma(perm, perm) == L \%*\% t(L)}.
+#' The permutation scheme is described in Genz and Bretz (2009) in Section 4.1.3, p.37.
+#' @param Sigma \code{d} by \code{d} covariance matrix
+#' @param l \code{d} vector of lower bounds
+#' @param u \code{d} vector of upper bounds
+#' @param method string indicating which method to use. Default to \code{"GGE"}
+#' @return a list with components
+#' \itemize{
+#' \item{\code{L}: }{Cholesky root}
+#' \item{\code{l}: }{permuted vector of lower bounds}
+#' \item{\code{u}: }{permuted vector of upper bounds}
+#' \item{\code{perm}: }{vector of integers with ordering of permutation}
+#' }
+#' @export
+#' @references Genz, A. and Bretz, F. (2009). Computations of Multivariate Normal and t Probabilities, volume 105. Springer, Dordrecht.
+#' @references Gibson G.J., Glasbey C.A. and D.A. Elton (1994).  Monte Carlo evaluation of multivariate normal integrals and sensitivity to variate ordering. In: Dimon et al., Advances in Numerical Methods and Applications, WSP, pp. 120-126.
+cholperm <- function(Sigma, l, u, method = c("GGE", "GB")){
+  method <- match.arg(method, c("GGE","GB"))[1]
+  if(method == "GGE"){
+    .cholpermGGE(Sigma = Sigma, l = l, u = u)
+  } else{
+    .cholpermGB(Sigma = Sigma, l = l, u = u)
   }
+}
--- R/mvNcdf.R
+++ R/mvNcdf.R
@@ -1,47 +1,78 @@
-mvNcdf <-
-  function(l,u,Sig,n){
-    ## truncated multivariate normal cumulative distribution
-    # computes an estimator of the probability Pr(l<X<u),
-    # where 'X' is a zero-mean multivariate normal vector
-    # with covariance matrix 'Sig', that is, X~N(0,Sig)
-    # infinite values for vectors 'u' and 'l' are accepted;
-    # Monte Carlo method uses sample size 'n'; the larger
-    # the 'n', the smaller the relative error of the estimator;
-    #      output: structure 'est' with
-    #              1. estimated value of probability Pr(l<X<u)
-    #              2. estimated relative error of estimator
-    #              3. theoretical upper bound on true Pr(l<X<u)
-    #              Remark: If you want to estimate Pr(l<Y<u),
-    #                   where Y~N(m,Sig) has mean vector 'm',
-    #                     then use 'mvNcdf(Sig,l-m,u-m,n)'.
-    # * Example:
-    #  d=25;l=rep(5,d);u=rep(Inf,d);
-    #  Sig=0.5*diag(d)+.5*matrix(1,d,d);
-    #  est=mvNcdf(l,u,Sig,10^4) # output of our method
-    #
-    # Reference:
-    # Z. I. Botev (2015),
-    # "The Normal Law Under Linear Restrictions:
-    #  Simulation and Estimation via Minimax Tilting",
-    #  submitted to JRSS(B)
+#' Truncated multivariate normal cumulative distribution
+#' 
+#' 
+#' Computes an estimate and a deterministic upper bound of the probability Pr\eqn{(l<X<u)},
+#' where \eqn{X} is a zero-mean multivariate normal vector 
+#' with covariance matrix \eqn{\Sigma}, that is, \eqn{X} is drawn from \eqn{N(0,\Sigma)}.
+#' Infinite values for vectors \eqn{u} and \eqn{l} are accepted. 
+#' The Monte Carlo method uses sample size \eqn{n}; 
+#' the larger \eqn{n}, the smaller the relative error of the estimator.
+#' 
+#' @param l lower truncation limit
+#' @param u upper truncation limit
+#' @param Sig covariance matrix of \eqn{N(0,\Sigma)}
+#' @param n number of Monte Carlo simulations
+#' @details Suppose you wish to estimate Pr\eqn{(l<AX<u)},
+#'  where \eqn{A} is a full rank matrix
+#'  and \eqn{X} is drawn from \eqn{N(\mu,\Sigma)}, then you simply compute
+#'  Pr\eqn{(l-A\mu<AY<u-A\mu)},
+#'  where \eqn{Y} is drawn from \eqn{N(0, A\Sigma A^\top)}.
+#' @return  a list with components
+#' \itemize{
+#' \item{\code{prob}: }{estimated value of probability Pr\eqn{(l<X<u)}}
+#' \item{\code{relErr}: }{estimated relative error of estimator} 
+#' \item{\code{upbnd}: }{ theoretical upper bound on true Pr\eqn{(l<X<u)}} 
+#' }  
+#' @author Zdravko I. Botev
+#' @export
+#' @keywords internal
+#' @references Z. I. Botev (2017), \emph{The Normal Law Under Linear Restrictions:
+#' Simulation and Estimation via Minimax Tilting}, Journal of the Royal
+#' Statistical Society, Series B, \bold{79} (1), pp. 1--24.
+#' 
+#' @note For small dimensions, say \eqn{d<50}, better accuracy may be obtained by using 
+#' the (usually slower) quasi-Monte Carlo version  \code{\link{mvNqmc}} of this algorithm.  
+#' @seealso \code{\link{mvNqmc}}, \code{\link{mvrandn}}
+#' @examples
+#' d <- 15; l <- 1:d; u <- rep(Inf, d);
+#' Sig <- matrix(rnorm(d^2), d, d)*2; Sig=Sig %*% t(Sig)
+#' mvNcdf(l, u, Sig, 1e4) # compute the probability 
+mvNcdf <-  function(l, u, Sig, n = 1e5){
     d=length(l); # basic input check
-    if  (length(u)!=d|d!=sqrt(length(Sig))|any(l>u)){
+    if  (length(u) !=d | d != sqrt(length(Sig)) | any(l > u)){
       stop('l, u, and Sig have to match in dimension with u>l')
     }
+    if(d == 1L){
+      #warning("Univariate problem not handled; using `pnorm`")
+      return(list(prob = exp(lnNpr(a = l / sqrt(Sig[1]), b = u / sqrt(Sig[1]))), err = NA, relErr = NA, upbnd = NA))
+    }
     # Cholesky decomposition of matrix
-    out=cholperm( Sig, l, u ); L=out$L; l=out$l; u=out$u; D=diag(L);
-    if (any(D<10^-10)){
+    out <- cholperm(Sig, l, u)
+    L <- out$L
+    l <- out$l
+    u <- out$u
+    D <- diag(L)
+    if (any(D < 1e-10)){
       warning('Method may fail as covariance matrix is singular!')
     }
-    L=L/D;u=u/D;l=l/D; # rescale
-    L=L-diag(d); # remove diagonal
+    L <- L / D
+    u <- u / D
+    l <- l / D # rescale
+    L <- L - diag(d) # remove diagonal
     # find optimal tilting parameter via non-linear equation solver
-    xmu<-nleq(l,u,L) # nonlinear equation solver 
-    #soln<-nleqslv(rnorm(2*d-2),gradpsi,jac=NULL,L,l,u,method = c("Broyden"))
-    #xmu<-soln$x;
-    x=xmu[1:(d-1)];mu=xmu[d:(2*d-2)]; # assign saddlepoint x* and mu*
-    est=mvnpr(n,L,l,u,mu);
+    x0 <- rep(0, 2 * length(l) - 2)
+    solvneq <- nleqslv::nleqslv(x0, fn = gradpsi, jac = jacpsi,
+                                L = L, l = l, u = u, global = "pwldog", method = "Broyden",
+                                control = list(maxit = 500L))
+    xmu <- solvneq$x
+    exitflag <- solvneq$termcd
+    if(!(exitflag %in% 1:2) || !isTRUE(all.equal(solvneq$fvec, rep(0, length(x0)), tolerance = 1e-6))){
+      warning('Did not find a solution to the nonlinear system in `mvrandn`!')
+    }
+    x <- xmu[1:(d-1)]
+    mu <- xmu[d:(2*d-2)] # assign saddlepoint x* and mu*
+    est <- mvnpr(n, L, l, u, mu)
     # compute psi star
-    est$upbnd=exp(psy(x,L,l,u,mu));
+    est$upbnd <- exp(psy(x, L, l, u, mu))
     return(est)
   }
